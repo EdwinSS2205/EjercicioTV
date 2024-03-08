22ae514c2089830fbde2c2a4e6d6d051e1cba06d
Estudiantes <efsaavedras@udistrital.edu.co>
2024-03-08 07:46:24 -0500

    Creando Tv

--- TelevisorEFS.java
+++ TelevisorEFS.java
@@ -6,37 +6,128 @@ public class TelevisorEFS
 	boolean encendido; 
 	int volumen;
 	int canal;
-	String entrada;
+	String[] entrada;
+
+	
+	public TelevisorEFS() 
+	{
+		this.volumen = 0;
+		this.canal = 0;
+		this.encendido = false;
+	}
+
+	
+	public void encender() 
+	{
+		this.encendido= true;
+	}
+	
+	
+	public void apagar() 
+	{
+		this.encendido= false;
+	}
 	
 	
 	
-	public boolean encender() 
+	public void VolumenMas() 
+	{
+		if (encendido && volumen < 100)
+		{
+		 volumen++;
+		}
+	}
+		
+	
+	
+	public void VolumenMenos() 
+	{
+		if(encendido && volumen >0) 
+		{
+		 volumen--;
+		}
+	}
+	
+	
+	public void CanalMas() 
+	{
+		if(encendido) 
+		{
+		canal++;
+		}
+	}
+	
+	
+	public void CanalMenos() 
+	{
+		if(encendido && canal>1) 
+		{
+		canal--;
+		}
+	}
+
+
+
+
+	public String getMarca() 
+	{
+		return marca;
+	}
+
+	public void setMarca(String marca) 
+	{
+		this.marca = marca;
+	}
+
+	public int getTamaño() 
+	{
+		return tamaño;
+	}
+
+	public void setTamaño(int tamaño) 
+	{
+		this.tamaño = tamaño;
+	}
+
+	public boolean isEncendido() 
 	{
 		return encendido;
 	}
-	
-	
-	public int VolumenMas() 
+
+	public void setEncendido(boolean encendido) 
 	{
-		return volumen++;
+		this.encendido = encendido;
+	}
+
+	public int getVolumen() 
+	{
+		return volumen;
+	}
+
+	public void setVolumen(int volumen) 
+	{
+		this.volumen = volumen;
+	}
+
+	public int getCanal()
+	{
+		return canal;
+	}
+
+	public void setCanal(int canal) 
+	{
+		this.canal = canal;
+	}
+
+	public String[] getEntrada() 
+	{
+		return entrada;
 	}
 	
-	
-	public int VolumenMenos() 
+	public void setEntrada(String[] entrada) 
 	{
-		return volumen--;
-	}
-	
-	
-	public int CanalMas() 
-	{
-		return canal++;
-	}
-	
-	
-	public int CanalMenos() 
-	{
-		return canal--;
+		this.entrada = entrada;
 	}	
+	
 	
 }
